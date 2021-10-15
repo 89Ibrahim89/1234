8ba3fb992f3f199f6fb258a6538ddad13f336d58
ibrahim <itolobaev@gmail.com>
2021-10-15 08:54:10 +0600

    kjh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList <String> listA = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> listA = new ArrayList<>();
+        ArrayList<String> listB = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Введите имя № " + i + " для списка Листа А");
+            listA.add(i, scanner.next());
+        }
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println("Введите имя №" + i + " для списка Листа Б");
+            listB.add(i, scanner.next());
+        }
+
+        listA = new ArrayList<>();
         listA.add("Асан");
         listA.add("Тамара");
         listA.add("Болот");
@@ -16,7 +26,9 @@ public class Main {
         listA.add("Апал");
         System.out.println("Список А: " + listA);
         System.out.println("------------------------------------------------");
-        ArrayList<String> listB = new ArrayList<>();
+
+
+        listB = new ArrayList<>();
         listB.add("Упол");
         listB.add("Байыш");
         listB.add("Бекен");
@@ -24,30 +36,39 @@ public class Main {
         listB.add("Усон");
         System.out.println("Список В: " + listB);
         System.out.println("-------------------------------------------------");
-        ArrayList<String> listC =new ArrayList<>();
-        listC.add(0,"Асан");
-        listC.add(1,"Усон");
-        listC.add(2,"Тамара");
-        listC.add(3,"Самара");
-        listC.add(4,"Болот");
-        listC.add(5,"Бекен");
-        listC.add(6,"Жаныш");
-        listC.add(7,"Байыш");
-        listC.add(8,"Апал");
-        listC.add(9,"Упол");
+
+
+        ArrayList<String> listC = new ArrayList<>();
+        listC.add(0, "Асан");
+        listC.add(1, "Усон");
+        listC.add(2, "Тамара");
+        listC.add(3, "Самара");
+        listC.add(4, "Болот");
+        listC.add(5, "Бекен");
+        listC.add(6, "Жаныш");
+        listC.add(7, "Байыш");
+        listC.add(8, "Апал");
+        listC.add(9, "Упол");
         System.out.println("Список С: " + listC);
 
+        listC = new ArrayList<>();
+        listC.addAll(listA);
 
 
-//        listA.addAll(listB);
-//        System.out.println("Список C: " + listA);
-//        ArrayList <String> listC = new ArrayList<>();
-//        System.out.println("Список С: " + listA + listB);
+        listC.add(1,listB.get(4));
+        System.out.println(listC);
+        listC.add(3,listB.get(3));
+        System.out.println(listC);
+        listC.add(5,listB.get(2));
+        System.out.println(listC);
+        listC.add(7,listB.get(1));
+        System.out.println(listC);
+        listC.add(9,listB.get(0));
+        System.out.println(listC);
 
-//        System.out.println(listA);
-//        Collections.sort(listA);
-//        System.out.println(listB);
-//        Collections.sort(listB);
+        listC.sort(Comparator.comparing(String::length));
+        System.out.println(listC);
+
 
     }
 }
